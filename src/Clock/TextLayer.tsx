f3e7b05f45dd3bbcda36f1741ce252c41b50e91e
mike castleman <m@mlcastle.net>
2021-03-09 19:53:53 -0500

    additional text layer format types

--- src/Clock/TextLayer.tsx
+++ src/Clock/TextLayer.tsx
@@ -1,81 +1,106 @@
 import * as React from 'react';
 import type { Property } from 'csstype';
-import { DateTimeFormatter } from '@js-joda/core';
+import {
+  ChronoField,
+  DateTimeFormatter,
+  TemporalField,
+  ZonedDateTime,
+} from '@js-joda/core';
 // @ts-ignore
 import { Locale } from '@js-joda/locale_en-us';
+import { toWords } from 'number-to-words';
 
 import { LayerProps } from './LayerProps';
 import { useTime } from '../TimeContext';
 import {
-  ClockLayerTextCasing,
-  ClockLayerTextJustification,
-  ClockLayerTextOptionsDateTimeFormat,
+  ClockLayerTextCasing as Casing,
+  ClockLayerTextJustification as Justification,
+  ClockLayerTextOptionsDateTimeFormat as Format,
   ClockLayerType,
 } from '../open-clock';
 
 interface DateTimeTextProps {
-  dateTimeFormat: ClockLayerTextOptionsDateTimeFormat;
+  dateTimeFormat: Format;
 }
 
-const p = (pattern: string): DateTimeFormatter =>
-  DateTimeFormatter.ofPattern(pattern).withLocale(Locale.US);
+type FormatFunction = (time: ZonedDateTime) => string;
+
+const p = (pattern: string): FormatFunction => {
+  const formatter = DateTimeFormatter.ofPattern(pattern).withLocale(Locale.US);
+  return (time) => formatter.format(time);
+};
+
+const w = (
+  unit: TemporalField,
+  singular?: string,
+  plural?: string
+): FormatFunction => (time) => {
+  const n = time.get(unit);
+  const words = toWords(n);
+  if (typeof singular === 'string' && typeof plural === 'string') {
+    return `${words} ${n === 1 ? singular : plural}`;
+  } else {
+    return words;
+  }
+};
 
 const formatPatterns: {
-  [K in ClockLayerTextOptionsDateTimeFormat]:
-    | DateTimeFormatter
-    | string
-    | undefined;
+  [K in Format]: FormatFunction | string | undefined;
 } = {
-  [ClockLayerTextOptionsDateTimeFormat.City]: undefined,
-  [ClockLayerTextOptionsDateTimeFormat.Colon]: ':',
-  [ClockLayerTextOptionsDateTimeFormat.Country]: undefined,
-  [ClockLayerTextOptionsDateTimeFormat.Da]: undefined,
-  [ClockLayerTextOptionsDateTimeFormat.Dadd]: undefined,
-  [ClockLayerTextOptionsDateTimeFormat.Dd]: undefined,
-  [ClockLayerTextOptionsDateTimeFormat.Ddauto]: undefined,
-  [ClockLayerTextOptionsDateTimeFormat.Ddmm]: p('dd MMM'),
-  [ClockLayerTextOptionsDateTimeFormat.Dl]: p('EEEE'),
-  [ClockLayerTextOptionsDateTimeFormat.Dw]: undefined,
-  [ClockLayerTextOptionsDateTimeFormat.Dy]: undefined,
-  [ClockLayerTextOptionsDateTimeFormat.Hh]: p('h'),
-  [ClockLayerTextOptionsDateTimeFormat.Hhmm]: p('h:mm'),
-  [ClockLayerTextOptionsDateTimeFormat.Hhmmpm]: p('h:mm a'),
-  [ClockLayerTextOptionsDateTimeFormat.Hhmmss]: p('h:mm:ss'),
-  [ClockLayerTextOptionsDateTimeFormat.HourWord]: undefined,
-  [ClockLayerTextOptionsDateTimeFormat.HourWordUnit]: undefined,
-  [ClockLayerTextOptionsDateTimeFormat.Ml]: undefined,
-  [ClockLayerTextOptionsDateTimeFormat.Mm]: undefined,
-  [ClockLayerTextOptionsDateTimeFormat.Mmdd]: undefined,
-  [ClockLayerTextOptionsDateTimeFormat.Mn]: undefined,
-  [ClockLayerTextOptionsDateTimeFormat.Mo]: undefined,
-  [ClockLayerTextOptionsDateTimeFormat.MinuteWord]: undefined,
-  [ClockLayerTextOptionsDateTimeFormat.MinuteWordUnit]: undefined,
-  [ClockLayerTextOptionsDateTimeFormat.Pm]: undefined,
-  [ClockLayerTextOptionsDateTimeFormat.Ss]: undefined,
-  [ClockLayerTextOptionsDateTimeFormat.SecondsWord]: undefined,
-  [ClockLayerTextOptionsDateTimeFormat.SecondsWordUnit]: undefined,
-  [ClockLayerTextOptionsDateTimeFormat.Slash]: '/',
-  [ClockLayerTextOptionsDateTimeFormat.Wy]: undefined,
-  [ClockLayerTextOptionsDateTimeFormat.Yy]: undefined,
-  [ClockLayerTextOptionsDateTimeFormat.Yyyy]: undefined,
+  [Format.City]: undefined,
+  [Format.Colon]: ':',
+  [Format.Country]: undefined,
+  [Format.Da]: p('eee'),
+  [Format.Dadd]: p('eee d'),
+  [Format.Dd]: p('dd'),
+  [Format.Ddauto]: p('d'),
+  [Format.Ddmm]: p('d MMM'),
+  [Format.Dl]: p('EEEE'),
+  [Format.Dw]: p('E'),
+  [Format.Dy]: p('D'),
+  [Format.Hh]: p('h'),
+  [Format.Hhmm]: p('h:mm'),
+  [Format.Hhmmpm]: p('h:mm a'),
+  [Format.Hhmmss]: p('h:mm:ss'),
+  [Format.HourWord]: w(ChronoField.HOUR_OF_AMPM),
+  [Format.HourWordUnit]: w(ChronoField.HOUR_OF_AMPM, 'hour', 'hours'),
+  [Format.Ml]: p('MMMM'),
+  [Format.Mm]: p('mm'),
+  [Format.Mmdd]: p('MMM d'),
+  [Format.Mn]: p('M'),
+  [Format.Mo]: p('MMM'),
+  [Format.MinuteWord]: w(ChronoField.MINUTE_OF_HOUR),
+  [Format.MinuteWordUnit]: w(ChronoField.MINUTE_OF_HOUR, 'minute', 'minutes'),
+  [Format.Pm]: p('a'),
+  [Format.Ss]: p('ss'),
+  [Format.SecondsWord]: w(ChronoField.SECOND_OF_MINUTE),
+  [Format.SecondsWordUnit]: w(
+    ChronoField.SECOND_OF_MINUTE,
+    'second',
+    'seconds'
+  ),
+  [Format.Slash]: '/',
+  [Format.Wy]: p('w'),
+  [Format.Yy]: p('yy'),
+  [Format.Yyyy]: p('yyyy'),
 };
 
 const textAnchors: {
-  [K in ClockLayerTextJustification]: Property.TextAnchor;
+  [K in Justification]: Property.TextAnchor;
 } = {
-  [ClockLayerTextJustification.Centered]: 'middle',
-  [ClockLayerTextJustification.Left]: 'start',
-  [ClockLayerTextJustification.Right]: 'end',
+  [Justification.Centered]: 'middle',
+  [Justification.Left]: 'start',
+  [Justification.Right]: 'end',
 };
 
 const textTransforms: {
-  [K in ClockLayerTextCasing]: Property.TextTransform;
+  [K in Casing]: Property.TextTransform;
 } = {
-  [ClockLayerTextCasing.Lower]: 'lowercase',
-  [ClockLayerTextCasing.None]: 'none',
-  [ClockLayerTextCasing.Sentence]: 'initial',
-  [ClockLayerTextCasing.Uppercased]: 'uppercase',
-  [ClockLayerTextCasing.Word]: 'capitalize',
+  [Casing.Lower]: 'lowercase',
+  [Casing.None]: 'none',
+  [Casing.Sentence]: 'initial',
+  [Casing.Uppercased]: 'uppercase',
+  [Casing.Word]: 'capitalize',
 };
 
 const DateTimeText: React.FunctionComponent<DateTimeTextProps> = ({
@@ -83,12 +108,12 @@ const DateTimeText: React.FunctionComponent<DateTimeTextProps> = ({
 }) => {
   const now = useTime();
   const pattern = formatPatterns[dateTimeFormat];
-  if (typeof pattern === 'string') {
-    return <>{pattern}</>;
-  } else if (pattern === undefined) {
+  if (pattern === undefined) {
     return null;
+  } else if (typeof pattern === 'string') {
+    return <>{pattern}</>;
   } else {
-    return <>{pattern.format(now)}</>;
+    return <>{pattern(now)}</>;
   }
 };
 
